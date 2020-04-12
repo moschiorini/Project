77ad71db6fe1f1c3901fd99fc129b867c23b1902
moschiorini <44140118+moschiorini@users.noreply.github.com>
2020-04-12 20:46:57 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,39 +1,116 @@
-function suma(){
-	var s;
-	const a = Number(document.getElementById("cheese").value);
-	const b = Number(document.getElementById("cheese1").value);
-	s = (a + b)* 25;
-	document.getElementById("rezultat").innerHTML = s;
-} 
+window.onload = () => findFilter('all');
 
-function plus(){
-	var a = Number(document.getElementById("cheese").value);
-	a += 1;
-    document.getElementById("cheese").value = a;
-} 
+const cat = document.getElementById('filter');
+const aa = document.createElement('div');
+const tov = document.getElementById('tovar');
+const sum = document.getElementById('sum');
+const dropMenu = document.getElementsByClassName('dropdown-menu');
+const cart = document.getElementsByClassName('table')[0];
 
-function minus(){
-    var a = Number(document.getElementById("cheese").value);
-    a -= 1;
-    document.getElementById("cheese").value = a;
+let costSum = 0;
+const category = ["Pizza", "Drinks", "Wrap", "Toppings"];
+const firm = ["Samsung", "Xiaomi", "LG", "Apple", "Vesta"];
+
+const summa = (s) => {
+    costSum += s;
+    sum.innerHTML = `Всего: ${costSum} $`;
+};
+
+for (let tov of category) {
+    let drop = document.createElement('div');
+    drop.innerHTML = `<a class="dropdown-item" onclick=findFilter('${tov}')>${tov}</a>`;
+    dropMenu[0].appendChild(drop);
+}
+;
+for (let tov of firm) {
+    let drop = document.createElement('div');
+    drop.innerHTML = `<a class="dropdown-item" onclick=findFilter('${tov}')>${tov}</a>`;
+    dropMenu[1].appendChild(drop);
+}
+;
+
+
+const findFilter = (value) => {
+    value = value + '';
+    tov.innerHTML = '';
+    console.log('ddddd ', value);
+    for (let i = 0; i < tovar.length; i++) {
+        if (value == tovar[i].category || value == tovar[i].firm || value == 'all') {
+            let tovCont = document.createElement('div');
+            tovCont.className = "tovContainer";
+            tovCont.innerHTML = `
+      <div class="card card-cascade narrower ">
+		        <div class="view view-cascade overlay">
+		          <img src='${tovar[i].img}' class=" img-fluid"
+		            alt="sample photo">
+		          <a>
+		            <div class="mask rgba-white-slight"></div>
+		          </a>
+		        </div>
+		        <div class="card-body card-body-cascade text-center">
+		         
+		          <h4 class="card-title">
+		            <strong>
+		              <p class="black-text" href="">${tovar[i].name} </p>
+		            </strong>
+		          </h4>
+		          
+		          <p class="card-text"> ${tovar[i].category}
+		          </p>
+		         
+		          <div class="card-footer px-1">
+		            <span class="float-left font-weight-bold">
+		              <strong>${tovar[i].cost}</strong>
+		            </span>
+		            <span class="float-right">
+		              <button onclick="summa(tovar[${i}].cost);tovar[${i}].onCart++" class="btn btn-success" style="margin-top: 5px;">
+						<i class="material-icons">shopping_cart</i>
+				 	</button>
+		            </span>
+		          </div>
+		        </div>		
+      </div>
+    `;
+            tov.appendChild(tovCont);
+        }
+    }
 }
 
 
-
-// show cart
-
-
- (function(){
- 	const cartInfo = document.getElementById('cart-info');
- 	const cart = document.getElementById('cart');
-
-
-
- 	cartInfo.addEventListener('click', function(){
-      cart.classList.toggle('show-cart');
- 	})
- })
-
-
-
-//add items to cart
+const isOnCart = () => {
+    cart.innerHTML = `
+	<thead class="bg-dark ">
+                <tr>
+                  <th scope="col" class="border-0 bg-dark">
+                    <div class="p-2 px-3 text-uppercase">Product</div>
+                  </th>
+                  <th scope="col" class="border-0 bg-dark">
+                    <div class="py-2 text-uppercase">Name</div>
+                  </th>
+                  <th scope="col" class="border-0 bg-dark">
+                   <img alt="" width="70" class="img-fluid rounded shadow-sm">
+                    <div class="py-2 text-uppercase"></div>
+                  </th>
+                  <th scope="col" class="border-0 bg-dark">
+                    <div class="py-2 text-uppercase">Price</div>
+                  </th>
+                  <th scope="col" class="border-0 bg-dark">
+                    <div class="py-2 text-uppercase">Quantity</div>
+                  </th>
+                </tr>
+              </thead>
+              
+	`;
+    for (let i = 0; i < tovar.length; i++) {
+        if (tovar[i].onCart) {
+            let tovCont = document.createElement('tr');
+            tovCont.innerHTML = `
+			     <td>${tovar[i].category}</td>
+			     <th>${tovar[i].name}</th>
+			     <th><img src="${tovar[i].img}" style="height: 100px; width: 100px"></th>
+			     <td>${tovar[i].cost}</td>
+			     <td>${tovar[i].onCart}</td>`;
+            cart.appendChild(tovCont);
+        }
+    }
+}
---
+++ js/script.js
@@ -0,0 +1,144 @@
+let tovar = [
+	{
+      "name": "Vegeterian",
+      "value": 12,
+      "cost": 75,
+      "onCart": 0,
+      "category": "Pizza",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/279-1300.jpg"
+    },
+    {
+      "name": "Funghi",
+      "value": 6,
+      "cost": 50,
+      "onCart": 0,
+      "category": "Pizza",
+      "firm": "Samsung",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/17-1300.jpg"
+    },
+    {
+      "name": "4 Cheeses ",
+      "value": 10,
+      "cost": 80,
+      "onCart": 0,
+      "category": "Pizza",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/15-1300.jpg"
+    },
+    {
+      "name": "Capricioasa",
+      "value": 7,
+      "cost" : 60,
+      "onCart": 0,
+      "category": "Pizza",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/844-1300.jpg"
+    },
+    {
+      "name": "Rancho",
+      "value": 10,
+      "cost" : 100,
+      "onCart": 0,
+      "category": "Pizza",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/16-1300.jpg"
+    },
+    {
+      "name": "Peperoni",
+      "value": 4,
+      "cost" : 100,
+      "onCart": 0,
+      "category": "Pizza",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/39-1300.jpg"
+    },
+    {
+      "name": "Lemonade",
+      "value": 5,
+      "cost" : 30,
+      "onCart": 0,
+      "category": "Drinks",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/445-1300.jpg"
+    },
+    {
+      "name": "Orange",
+      "value": 4,
+      "cost" : 80,
+      "onCart": 0,
+      "category": "Drinks",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/446-1300.jpg"
+    },
+    {
+      "name": "Mors",
+      "value": 4,
+      "cost" : 80,
+      "onCart": 0,
+      "category": "Drinks",
+      "firm": "LG",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/75-1300.jpg"
+    },
+    {
+      "name": "Soda",
+      "value": 4,
+      "cost" : 80,
+      "onCart": 0,
+      "category": "Drinks",
+      "firm": "LG",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/419-1300.jpg"
+    },
+    {
+      "name": "Cola",
+      "value": 4,
+      "cost" : 80,
+      "onCart": 0,
+      "category": "Drinks",
+      "firm": "LG",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/53-1300.jpg"
+    },
+    {
+      "name": "Beer",
+      "value": 4,
+      "cost" : 80,
+      "onCart": 0,
+      "category": "Drinks",
+      "firm": "LG",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/421-1300.jpg"
+    },
+    {
+      "name": "Fried potatoes",
+      "value": 4,
+      "cost" : 20,
+      "onCart": 0,
+      "category": "Wrap",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/247-1300.jpg"
+    },
+    {
+      "name": "Potatoes",
+      "value": 4,
+      "cost" : 25,
+      "onCart": 0,
+      "category": "Wrap",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/247-1300.jpg"
+    },
+    {
+      "name": "Tsesari",
+      "value": 4,
+      "cost" : 20,
+      "onCart": 0,
+      "category": "Wrap",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/506-1300.jpg"
+    },
+    {
+      "name": "Sandwich",
+      "value": 4,
+      "cost" : 20,
+      "onCart": 0,
+      "category": "Wrap",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/538-1300.jpg"
+    },
+    {
+      "name": "Sandwich",
+      "value": 4,
+      "cost" : 20,
+      "onCart": 0,
+      "category": "Wrap",
+      "img": "https://static.pizzasushiwok.ru/images/menu_new/538-1300.jpg"
+    },
+
+];
